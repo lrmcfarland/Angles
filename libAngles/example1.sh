3dd37e5f8840621e822e338c4bc2c184a116f462
L.R. McFarland <lrm@bayarea.net>
2014-10-25 20:07:33 -0700

    updates README.mds

---
+++ libAngles/example1.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#
+# Shell wrapper to set up library path for sim
+#
+# TODO: add linux support
+#
+
+if [ -z "$ANGLES_ROOT" ]; then
+    ANGLES_ROOT=../
+    echo "# ANGLES_ROOT not set. Using" $ANGLES_ROOT
+else
+    echo "# ANGLES_ROOT is" $ANGLES_ROOT
+fi
+
+ANGLE_LIBRARY_PATH=${ANGLES_ROOT}/libAngles
+
+# if OSX
+DYLD_LIBRARY_PATH=${ANGLES_LIBRARY_PATH} ${ANGLES_ROOT}/libAngles/example1 "$@"
+
+# EoF
